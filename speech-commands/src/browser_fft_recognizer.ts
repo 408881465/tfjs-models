e44f92997699e760e9fad56524808998a8a6e13c
408881465 <36319865+408881465@users.noreply.github.com>
2024-02-16 15:02:45 +0800

    Update browser_fft_recognizer.ts

--- speech-commands/src/browser_fft_recognizer.ts
+++ speech-commands/src/browser_fft_recognizer.ts
@@ -61,7 +61,7 @@ export class BrowserFftSpeechCommandRecognizer implements
   static readonly DEFAULT_VOCABULARY_NAME = '18w';
 
   readonly MODEL_URL_PREFIX =
-      `https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/v${
+      `https://www.sjaiedu.com/tfjs-models/tfjs/speech-commands/v${
           getMajorAndMinorVersion(version)}/browser_fft`;
  // readonly MODEL_URL_PREFIX = model_url_prefix + getMajorAndMinorVersion(version_1.version) + "/browser_fft";
 
